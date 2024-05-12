2ccc561ad464e88b1a8450fa12709964997a82d6
bstroebl <nospam@stroweb.de>
2024-05-12 13:04:06 +0200

    [BUG] update sphinx-Extension [gitignore] Ignore files in help/build/

--- help/source/conf.py
+++ help/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '../../..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.imgmath', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
